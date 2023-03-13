a5ad0cf7f8e5a91f29aa44e5238a1c80182b0815
kickduck <kilianmatignon@gmail.com>
2023-03-13 05:18:40 +0100

    logo home cliquable

--- src/components/layout/header/header.js
+++ src/components/layout/header/header.js
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <header className='header'>
             <h1>
-                <img src={Logo} alt="Kasa, location d’appartements entre particuliers en France"></img>
+                <a href='/'><img src={Logo} alt="Kasa, location d’appartements entre particuliers en France"></img></a>
             </h1>
             <Navbar />
         </header>
