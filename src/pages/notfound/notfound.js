4576157a9137f8c3bab292eeb598a0366c8f8fad
kickduck <kilianmatignon@gmail.com>
2023-03-27 04:22:26 +0200

    notfound modif

--- src/pages/notfound/notfound.js
+++ src/pages/notfound/notfound.js
@@ -2,13 +2,13 @@ import './notfound.scss'
 import Header from '../../components/layout/header/header'
 export default function notfound() {
     return (
-        <div className='notfound'>
+        <>
             <Header />
             <section className="error">
                 <h1>404</h1>
                 <h2>Oups! La page que vous demandez n'existe pas.</h2>
                 <a href="/">Retourner sur la page d'accueil</a>
             </section>
-        </div>
+        </>
     )
 }
